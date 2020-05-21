ded9df4d965b31d79f125ed656cde7b8af9f30da
alk831 <adam54111@gmail.com>
2020-05-21 20:15:16 +0200

    create configure store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,22 @@
-import { createStore } from 'redux';
-import { rootReducer } from './modules';
+import { createStore, Store } from 'redux';
+import { rootReducer, StoreState } from './modules';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-export const store = createStore(rootReducer);
+export const configureStore = (
+  initialState?: Partial<StoreState>,
+): Store<StoreState> => {
+  const store = createStore(
+    rootReducer,
+    initialState,
+    composeWithDevTools(),
+  );
+
+  module.hot?.accept(() => {
+    const { rootReducer } = require('./modules');
+    store.replaceReducer(rootReducer);
+  });
+
+  return store;
+};
+
+export const store = configureStore();
